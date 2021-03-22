3f410a71c1786954986135659003f101cd6d43b2
itRianone <timokhindimitry@gmail.com>
2021-03-22 18:21:47 +0300

    optimization end

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
       autoplaySpeed: 2500,
       dots: true,
     }
-    );
+  );
     
     $('.specialties').slick(
       {
@@ -27,7 +27,7 @@ $(document).ready(function () {
         autoplaySpeed: 2500,
         dots: true,
       }
-      );
+    );
       
     $.fancybox.defaults.loop = "false";
 
@@ -35,46 +35,35 @@ $(document).ready(function () {
 
 function toggleClass() {
   let navigation = document.querySelectorAll('.header__nav');
-  //console.log(menu);
-  //let menu2 = document.querySelector('.qqe');
-  //let navBar = document.querySelector('.head__nav');
   for (let navItem of navigation) {
     
     navItem.classList.toggle('toggle__class');
 
   }
-  //menu.classList.toggle('toggle__class');
-  //menu2.classList.toggle('toggle__class');
 };
 
 
 function switchTeamPopup(id) {
   document.querySelector('.team-popup' + id).classList.toggle('active')
-  // document.querySelectorAll('.specialties__popup').classList.toggle('active')
 };
 
 function switchSpecPopup(id) {
   document.querySelector('.specialties-popup' + id).classList.toggle('active')
-  // document.querySelectorAll('.specialties__popup').classList.toggle('active')
 };
 
 function switchStyle(event, cityName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="menu__dishes" and hide them
   tabcontent = document.getElementsByClassName("menu__dishes");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   
-  // Get all elements with class="menu__tab-item" and remove the class "active"
   tablinks = document.getElementsByClassName("menu__tabs-wrapper");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   event.currentTarget.className += " active";
 }
